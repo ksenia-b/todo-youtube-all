0d196c3aaf563749bfe0c68d893a3ada6c8ea2ef
Oksana <ksenia.barshchyk@yandex.ru>
2021-04-19 19:40:09 +0300

    test external ip

--- app.py
+++ app.py
@@ -127,4 +127,4 @@ def delete_task(id):
     return make_response(jsonify("Deleted!"), 200)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port="5000", host="104.154.119.247")
